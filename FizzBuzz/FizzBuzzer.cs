99482af5a33cd11de80dc8a1ea9c372df99c7900
sirshelley <sirshelley@hotmail.co.uk>
2016-07-06 21:26:46 +0200

    Jamie Shelley Version
    .Completed FizzBuzz String Check Function
    .Added unit tests
    .Minor fiddling

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace FizzBuzz
 {
     public class FizzBuzzer : IFizzBuzzer
     {
+
+        //real simples, feel like i'm missing something?..
+        //Wanting to include validation; but that would void testcase exceptions I think
         public string FizzBuzzRange(int start, int end)
-        {
-            throw new NotImplementedException();
+        {          
+            string result = "";
+            
+            for (int i = start; i <= end; i++) //record all fiz, buzz or fizzbuzz
+            {
+                string tmp = i.ToString();
+
+                if ((i % 3) == 0 && (i % 5) != 0)
+                {
+                    tmp = "fizz";
+                }
+                if ((i % 5) == 0 && (i % 3) != 0)
+                {
+                    tmp = "buzz";
+                }
+                if ((i % 5) == 0 && (i % 3) == 0)
+                {
+                    tmp = "fizzbuzz";
+                }
+                result += tmp;
+            }
+            return result;
         }
     }
 }
