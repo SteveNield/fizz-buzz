847a9d4b98e444c60bf749a51a73e73f1f4ad215
Zach Rachedi <zotch77@gmail.com>
2017-06-29 02:14:41 +0100

    TimeTable

---
+++ FizzBuzz/Population.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Population
+    {
+        public TupleStruct[][][] Initialize()
+        {
+            Random rand = new Random();
+            for (int p = 0; p < Program.populationSize; p++)
+            {
+                Program.initialPopulation[p] = new TupleStruct[Program.nbrPeriods][];
+                for (int i = 0; i < Program.nbrPeriods; i++)
+                {
+                    Program.initialPopulation[p][i] = new TupleStruct[Program.nbrGroups];
+                    for (int j = 0; j < Program.nbrGroups; j++)
+                    {
+                        Program.initialPopulation[p][i][j].teacher = Program.teachers[rand.Next(0, Program.teachers.Length)];
+                        Program.initialPopulation[p][i][j].room = Program.rooms[rand.Next(0, Program.rooms.Length)];
+                    }
+                }
+            }
+            return Program.initialPopulation;
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,17 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+
+struct TupleStruct
+{
+    public string teacher, room;
+};
+
+
 namespace FizzBuzz
 {
     class Program
     {
+        public static int populationSize = 10;
+        public static int nbrPeriods = 20;
+        public static int nbrGroups = 5;
+        public static string[] teachers = { "T1", "T2", "T3", "T4", "T5" };
+        public static string[] rooms = { "R1", "R2", "R3", "R4", "R5" };
+        public static TupleStruct[][][] initialPopulation = new TupleStruct[populationSize][][];
+        public static TupleStruct[][][] offspringPopulation = new TupleStruct[populationSize][][];
+
         static void Main(string[] args)
         {
-            FizzBuzzer fizzBuzzer = new FizzBuzzer();
+            Chromosome genotype = new Chromosome();
+            Population population = new Population();
+            Fitness fitness = new Fitness();
+            Parent parent = new Parent();
+            Mutation mutation = new Mutation();
 
-            //Console.WriteLine(fizzBuzzer.FizzBuzzRange(8, 16));
+            initialPopulation = population.Initialize();
 
-            Console.WriteLine(fizzBuzzer.FizzBuzzRange(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()))); //Win for readability!! But yeah.. we both know you're not interested in this class
+            TupleStruct[][] fittestGenotype = initialPopulation[0];
+            int currentGeneration = 0;
+            int maxGenerations = 1000;
+
+            while (currentGeneration < maxGenerations && fitness.Evaluate(fittestGenotype) > 0)
+            {
+                for (int i = 0; i < populationSize; i++)
+                {
+                    offspringPopulation[i] = mutation.Mutate(parent.Selection(initialPopulation));
+                    if (fitness.Evaluate(offspringPopulation[i]) < fitness.Evaluate(fittestGenotype))
+                    {
+                        fittestGenotype = offspringPopulation[i];
+                    }
+                }
+
+                initialPopulation = offspringPopulation;
+                currentGeneration++;
+                Console.WriteLine("Generation {0}", currentGeneration);
+            }
+            Console.WriteLine("\nFittest genotype :");
+            genotype.Print(fittestGenotype);
+            Console.WriteLine("Fitness : {0}\n", fitness.Evaluate(fittestGenotype));
 
         }
     }
